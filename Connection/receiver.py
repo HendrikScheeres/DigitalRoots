2eace7f5deacb34273b8c9f0e3a006a664e4d050
HendrikScheeres <you@example.com>
2023-12-01 19:30:34 +0100

    ...

--- Connection/receiver.py
+++ Connection/receiver.py
@@ -1,9 +1,7 @@
-#%%
 import socket
-import time
 
 # Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Get local machine name and port
 host = socket.gethostname()
@@ -17,41 +15,16 @@ print(f"Port: {port}")
 ip_address = socket.gethostbyname(host)
 print(f"IPv4 Address: {ip_address}")
 
-#%% FUNCTIONS
-def receive_data(ip, port):
-    # create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # bind the socket to the port
-    s.bind((ip, port))
-    # listen for incoming connections
-    s.listen(1)
-    # accept the connection
-    conn, addr = s.accept()
-    # print the address of the connection
-    print("Connection address:", addr)
-    # receive the data
-    data = conn.recv(1024)
-    # decode the data
-    data = data.decode()
-    # print the data
-    print("Received data:", data)
-    # close the connection
-    conn.close()
-    # return the data
-    return data
+# Connect to the server
+client_socket.connect((host, port))
 
-#%% MAIN
+# Send data to server
+message_to_send = 'Hello, server! This is the client.'
+client_socket.send(message_to_send.encode())
 
-if __name__ == "__main__":
-    # define the ip and port
-    computer1_ip = "25.42.216.73"   # Replace with the actual IP of computer1
-    computer1_port = 12345
+# Receive response from server
+data_received = client_socket.recv(1024)
+print('Received from server:', data_received.decode())
 
-    # Start a while loop and print the data received every 10 seconds
-    while True:
-        # receive the data
-        data = receive_data(computer1_ip, computer1_port)
-        # wait for 3 seconds
-        time.sleep(3)
-        # print the data
-        print(data)
+# Close the connection
+client_socket.close()
