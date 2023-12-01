88b25e4c3c5a23cf4df70b649c8fe1821d70de85
HendrikScheeres <hscheeresyt@gmail.com>
2023-12-01 19:30:58 +0100

    ...

--- Connection/receiver.py
+++ Connection/receiver.py
@@ -17,29 +17,6 @@ print(f"Port: {port}")
 ip_address = socket.gethostbyname(host)
 print(f"IPv4 Address: {ip_address}")
 
-#%% FUNCTIONS
-def receive_data(ip, port):
-    # create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # bind the socket to the port
-    s.bind((ip, port))
-    # listen for incoming connections
-    s.listen(1)
-    # accept the connection
-    conn, addr = s.accept()
-    # print the address of the connection
-    print("Connection address:", addr)
-    # receive the data
-    data = conn.recv(1024)
-    # decode the data
-    data = data.decode()
-    # print the data
-    print("Received data:", data)
-    # close the connection
-    conn.close()
-    # return the data
-    return data
-
 #%% MAIN
 
 if __name__ == "__main__":
@@ -50,7 +27,8 @@ if __name__ == "__main__":
     # Start a while loop and print the data received every 10 seconds
     while True:
         # receive the data
-        data = receive_data(computer1_ip, computer1_port)
+        data = server_socket.recv(1024)
+
         # wait for 3 seconds
         time.sleep(3)
         # print the data
