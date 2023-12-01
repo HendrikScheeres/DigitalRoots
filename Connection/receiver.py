4580dea2bcf6908d40296205c0d546063c9df89b
HendrikScheeres <hscheeresyt@gmail.com>
2023-12-01 18:02:53 +0100

    Comments

--- Connection/receiver.py
+++ Connection/receiver.py
@@ -39,7 +39,7 @@ def receive_data(ip, port):
 
 if __name__ == "__main__":
     # define the ip and port
-    ip = "62.163.195.37"
+    ip = "192.168.178"
     port = 12345
 
     # Start a while loop and print the data received every 10 seconds
