dad4327ffe286f46942b289d3395b6b9bc855157
HendrikScheeres <you@example.com>
2023-12-05 21:19:34 +0100

    Some arduino things

--- Arduino_Sketch/Arduino_Sketch.ino
+++ Arduino_Sketch/Arduino_Sketch.ino
@@ -11,18 +11,11 @@ void loop() {
     char receivedChar = Serial.read();
     
     if (receivedChar == '1') {
-      blinkDelay = 1000;  // Set blink delay to 1 second
-      blinkLED();
+      digitalWrite(ledPin, HIGH);
     } else if (receivedChar == '2') {
-      blinkDelay = 500;  // Set blink delay to 0.5 seconds (twice per second)
-      blinkLED();
+      digitalWrite(ledPin, LOW);
     }
   }
 }
 
-void blinkLED() {
-  digitalWrite(ledPin, HIGH);
-  delay(blinkDelay);
-  digitalWrite(ledPin, LOW);
-  delay(blinkDelay);
-}
+
